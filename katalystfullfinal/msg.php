f023cd61c2b864df683e35c5feee9224620b01ba
HarshGrandeur <hrshgpt73@gmail.com>
2016-07-24 00:19:51 -0400

    Update msg.php

--- katalystfullfinal/msg.php
+++ katalystfullfinal/msg.php
@@ -1,7 +1,8 @@
 <?php
-    include('way2sms-api.php');
+   
     function msg($num,$msg)
     {
+    include('way2sms-api.php');
     sendWay2SMS ( '9920178153' , '5322' , $num , $msg);   
   }
      
